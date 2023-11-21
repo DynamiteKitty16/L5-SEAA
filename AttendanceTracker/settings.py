31f96e3ccff80b5d40be6dc258e4dce7add03ca2
Katie Boal <145475122+DynamiteKitty16@users.noreply.github.com>
2023-11-21 01:29:10 +0000

    adjusting templates location again

--- AttendanceTracker/settings.py
+++ AttendanceTracker/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'AttendanceTracker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'tracker/templates'],
+        'DIRS': [BASE_DIR / 'tracker' / 'templates' / 'tracker'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
