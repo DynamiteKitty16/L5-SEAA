8e0b670d5cc9ebed0ab0853b33128385780c8c1f
Katie Boal <145475122+DynamiteKitty16@users.noreply.github.com>
2023-12-05 23:47:39 +0000

    clean up forms and include clean email to CustomUserCreation

--- tracker/forms.py
+++ tracker/forms.py
@@ -8,11 +8,10 @@ from django.core.validators import RegexValidator
 # Define any regex validators for Django
 name_regex = RegexValidator(r'^[a-zA-Z\s]+$', 'Only letters and spaces are allowed.')
 
-# Putting CustomUserCreationForm ahead as UserProfileForm below is a dependency
 class CustomUserCreationForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, validators=[name_regex])
     last_name = forms.CharField(max_length=30, validators=[name_regex])
-    email = forms.EmailField(required=True, unique=True)
+    email = forms.EmailField(required=True)
 
     class Meta:
         model = User
@@ -24,6 +23,12 @@ class CustomUserCreationForm(UserCreationForm):
         self.fields['password1'].help_text = 'Your password must be at least 8 characters long, contain at least one number, one uppercase letter, and one special character.'
         self.fields['password2'].help_text = 'Enter the same password as before, for verification.'
 
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("This email address address is already in use.")
+        return email
+    
     def save(self, commit=True):
         user = super(CustomUserCreationForm, self).save(commit=False)
         first_name = self.cleaned_data['first_name']
@@ -47,33 +52,6 @@ class CustomUserCreationForm(UserCreationForm):
         if commit:
             user.save()
         return user
-
-
-class UserProfileForm(forms.ModelForm):
-    first_name = forms.CharField(max_length=30, validators=[name_regex])
-    last_name = forms.CharField(max_length=30, validators=[name_regex])
-    email = forms.EmailField()
-
-    def clean_email(self):
-        email = self.cleaned_data['email']
-        if User.objects.filter(email=email).exists():
-            raise forms.ValidationError("This email address is already in use.")
-        return email
-
-    class Meta:
-        model = UserProfile
-        fields = ['first_name', 'last_name', 'email']
-
-    def save(self, commit=True):
-        user_profile = super(UserProfileForm, self).save(commit=False)
-        user = user_profile.user
-        user.first_name = self.cleaned_data['first_name']
-        user.last_name = self.cleaned_data['last_name']
-        user.email = self.cleaned_data['email']
-        if commit:
-            user.save()
-            user_profile.save()
-        return user_profile
     
 class AttendanceRecordForm(forms.ModelForm):
     class Meta:
