f1597800c1b828dde9e7333cfefbdc5cd370ffcc
Katie Boal <145475122+DynamiteKitty16@users.noreply.github.com>
2023-12-15 23:39:32 +0000

    updated the middleware file as this held the json response issue for datetime

--- AttendanceTracker/middleware.py
+++ AttendanceTracker/middleware.py
@@ -11,13 +11,14 @@ class InactivityTimeoutMiddleware:
         if request.user.is_authenticated:
             # Get the last activity time from the session
             last_activity = request.session.get('last_activity')
+            current_time = timezone.now().timestamp() # added in a UNIX conversion due to JSON string issue
 
             # If last_activity is not set, or the user has been inactive for SESSION_COOKIE_AGE seconds
-            if not last_activity or (timezone.now() - last_activity).total_seconds() > settings.SESSION_COOKIE_AGE:
+            if not last_activity or (current_time - last_activity) > settings.SESSION_COOKIE_AGE:
                 logout(request)  # Log out the user
 
         # Update last activity time in the session
-        request.session['last_activity'] = timezone.now()
+        request.session['last_activity'] = current_time # changing this stores timestamp
 
         response = self.get_response(request)
         return response
