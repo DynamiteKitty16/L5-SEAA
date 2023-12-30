a232853b98867e903cac26b32bab367b789720b1
Katie Boal <145475122+DynamiteKitty16@users.noreply.github.com>
2023-12-30 20:41:41 +0000

    removed duplicate model in admin and updated current one

--- tracker/admin.py
+++ tracker/admin.py
@@ -7,19 +7,16 @@ admin.site.register(AttendanceRecord)
 admin.site.register(LeaveRequest)
 
 # Assing users as managers through django interface
+# Ensure only managers show in the manager dropdown
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
+    form = UserProfileAdminForm
     list_display = ['user', 'manager', 'is_manager']
     list_editable = ['manager']
     search_fields = ['user__username', 'manager__username']
-
-# Ensure only managers show in the manager dropdown
-class UserProfileAdmin(admin.ModelAdmin):
-    form = UserProfileAdminForm
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == "manager":
             kwargs["queryset"] = User.objects.filter(is_manager=True)
         return super().formfield_for_foreignkey(db_field, request, **kwargs)
-
-admin.site.register(UserProfile, UserProfileAdmin)
+    
