08fdd82ab0c5585e3b06801bd8eedb27fac6911a
Katie Boal <145475122+DynamiteKitty16@users.noreply.github.com>
2024-01-03 02:21:13 +0000

    updated typo to import

--- AttendanceTracker/urls.py
+++ AttendanceTracker/urls.py
@@ -23,7 +23,7 @@ from django.urls import path
 from tracker import views
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import CustomPasswordResetConfirmView
+from tracker.views import CustomPasswordResetConfirmView
 
 
 urlpatterns = [
