59712e6aba31cbc348e094b62da9ceea0b0a4556
Katie Boal <145475122+DynamiteKitty16@users.noreply.github.com>
2024-01-03 02:48:52 +0000

    removed duplicate url for custom view

--- AttendanceTracker/urls.py
+++ AttendanceTracker/urls.py
@@ -36,9 +36,6 @@ urlpatterns = [
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(
         template_name='tracker/password_reset_done.html'
     ), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
-        template_name='tracker/password_reset_confirm.html'
-    ), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
         template_name='tracker/password_reset_complete.html'
     ), name='password_reset_complete'),
